7291e7a79787d5043b6442c07080908f5d86cb1b
DocIsIn <166623003+DocIsIn@users.noreply.github.com>
2024-04-17 19:21:00 -0400

    Update ChestItem.java

--- src/RPGGame/Item/Abstracts/ChestItem.java
+++ src/RPGGame/Item/Abstracts/ChestItem.java
@@ -2,4 +2,25 @@ package RPGGame.Item.Abstracts;
 
 public abstract class ChestItem extends EquipabbleItem {
   private String itemName; 
+  private int defenseRate;
+
+  public ChestItem(String itemName, int defenseRate){ 
+    this.itemName = itemName
+    this.defenseRate = defenseRate;
+  }
+  public String getItemName() {
+    return itemName;
+  }
+  public int getDefenseRate() { 
+    return defenseRate;
+  }
+  public void displayItem() {
+    System.out.println("Item Name: " + itemName + ", Defense Rate: +" + defenseRate);
 }
+
+class ChestItem {
+  public static void main(String [] args) {
+    ChestItem ironBreastPlate = new ChestItem("Iron BreastPlate", 2);
+    ChestItem bronzeBreastPlate = new ChestItem("Bronze BreastPlate", 1);
+    
+    ironBreastPlate.displayItem();
