3c4af80aa728d497a7774d05a8f3041008915f0e
DocIsIn <166623003+DocIsIn@users.noreply.github.com>
2024-04-26 21:15:26 -0400

    Update EventCaveEntrance.java

--- src/RPGGame/Event/Concrete/StoryEvents/EventCaveEntrance.java
+++ src/RPGGame/Event/Concrete/StoryEvents/EventCaveEntrance.java
@@ -1,5 +1,5 @@
 package RPGGame.Event.Concrete.StoryEvents;
-
+import RPGGame.Items;
 import RPGGame.Controller.PrimaryScene;
 import RPGGame.Player;
 import RPGGame.Event.Abstracts.Choice;
@@ -9,11 +9,12 @@ public class EventCaveEntrance extends StoryEvent {
     EventCaveEntrance(){
         super();
         this.setEventText("""
-                Success!! The river slowed down and you were able to cross. You come across a dark cave entrance and bats start to fly out of the cave.
-        this.setReencounterText("""
-                The cave entrance looms before you, its darkness inviting yet foreboding.
+                Success!! The river slowed down and you were able to cross. he air around it is cool and damp, and you can hear the echo of water dripping from within.
+                There's an eerie feeling about the place, but something draws you toward it.
                 """);
-        this.addChoice(new Choice("Enter the cave") {
+        this.setReencounterText("""
+                The cave entrance looms before you, its darkness inviting yet foreboding The sound of bats echoes from within and they start to attack you!""");
+        this.addChoice(new Choice("Fight the bats ") {
             @Override
             public void execute(Player player, PrimaryScene scene) {
                 scene.gameOutput("You step into the cave, and the darkness envelops you. The only sound is your echoing footsteps as you venture deeper into the unknown.");
