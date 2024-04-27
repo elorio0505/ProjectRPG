bab50f227510953e037e5099594bfe956e499bb5
DocIsIn <166623003+DocIsIn@users.noreply.github.com>
2024-04-27 12:01:12 -0400

    Update EventCaveEntrance.java

--- src/RPGGame/Event/Concrete/StoryEvents/EventCaveEntrance.java
+++ src/RPGGame/Event/Concrete/StoryEvents/EventCaveEntrance.java
@@ -27,7 +27,7 @@ public class EventCaveEntrance extends StoryEvent {
             public void execute(Player player, PrimaryScene scene) {
                 if (player.hasItem(Items.KNIFE)) {
                     scene.gameOutput("You are surrounded by the creatures, and a chaotic battle ensues. After a long hard fought, you killed all the mutant bats in the cave and continue on your journey");
-                    // Implement consequences of entering the cave and fighting the bats with the knife.
+                     new EventSecretTunnel().run(player, scene, false);
                 } else {
                     scene.gameOutput("Without a weapon, you are quickly overwhelmed by the swarm of bats.");
                     player.die("Swarm of mutant bats ate you alive", scene);
