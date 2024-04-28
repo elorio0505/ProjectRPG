3efffb089ef53fb99f478aee61cf2fc95ed8e952
DocIsIn <166623003+DocIsIn@users.noreply.github.com>
2024-04-28 02:20:02 -0400

    Update EventCaveSystem.java

--- src/RPGGame/Event/Concrete/StoryEvents/EventCaveSystem.java
+++ src/RPGGame/Event/Concrete/StoryEvents/EventCaveSystem.java
@@ -7,20 +7,28 @@ import RPGGame.Event.Abstracts.Choice;
 import RPGGame.Event.Abstracts.StoryEvent;
 
 public class EventLostCave extends StoryEvent {
-    EventLostCave(){
+    EventCaveSystem(){
         super();
         this.setEventText("""
                 You slip on loose gravel and tumble into a dark cavern. After regaining your bearings, you realize you are in a vast cave system, with no immediate way out.
                 The cave is eerily quiet, and your only source of light is a dimly glowing moss on the walls. You must find a way out before it's too late.
                 """);
         this.setReencounterText("""
-                The cave's twists and turns seem endless. Will you ever find your way out?
+                The cave's twists and turns seem endless. But there's two distinct paths with dimly glowing moss. What will you do?
                 """);
-        this.addChoice(new Choice("Search for a way out") {
+        this.addChoice(new Choice("Search the left path of the cave") {
             @Override
             public void execute(Player player, PrimaryScene scene) {
-                scene.gameOutput("You begin to explore the cave, hoping to find an exit. The moss-covered walls and eerie silence make your journey unsettling.");
-                // Implement consequences of searching for a way out (e.g., encountering obstacles, finding clues)
+                scene.gameOutput("You begin to explore the left path of the cave, hoping to find an exit. The moss-covered walls and eerie silence make your journey unsettling.");
+                new EventCaveSystemLeft().run(player, scene, true);
+            }
+        });
+
+        this.addChoice(new Choice("Search the right path of the cave") {
+            @Override
+            public void execute(Player player, PrimaryScene scene) {
+                scene.gameOutput("You begin to explore the right path cave, hoping to find an exit. The moss-covered walls and eerie silence make your journey unsettling.");
+                new EventCaveSystemRight().run(player, scene, true);
             }
         });
       
