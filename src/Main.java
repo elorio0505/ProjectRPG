7aaf06030495bfab6ae7ab5a1321c79b7ecbb857
DocIsIn <166623003+DocIsIn@users.noreply.github.com>
2024-04-30 14:43:15 -0400

    Added comments to Primary scene. Everything still runs

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import RPGGame.Controller.GameController;
 
 public class Main {
     public static void main(String[] args) {
-        GameController game = new GameController();
-        game.startGame();
+        GameController game = new GameController();    // Create an instance of GameController
+        game.startGame();             // Start the game by calling the startGame method on the GameController instance
     }
 }
